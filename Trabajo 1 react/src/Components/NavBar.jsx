60f6e6d190c64bcebd721be57d71ad991b8e16a0
pedro <pjaranzazu@correo.iue.edu.co>
2024-07-23 22:03:37 -0500

    first commit

---
+++ Trabajo 1 react/src/Components/NavBar.jsx
@@ -0,0 +1,32 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function NavBar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">La tienda de pepito</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Productos</Nav.Link>
+            <NavDropdown title="Categorias" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Alcohol</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Patinetas</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Panaderia</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Cosas extra
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
